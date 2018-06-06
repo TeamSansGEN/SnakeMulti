7dbfb9cdd0eca94edab94b346e2aa3227b0274ef
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-06-06 11:12:14 +0200

    Game constants modified on both server and client

--- Libgdx/core/src/gen/snakemulti/GameConstants.java
+++ Libgdx/core/src/gen/snakemulti/GameConstants.java
@@ -2,7 +2,8 @@ package gen.snakemulti;
 
 public class GameConstants {
 
-    public static final double PIXELS_BY_TILE = 8;
+    public static final double PIXELS_BY_TILE =     8;
+    public static final int    BUFF_SIZE      = 32768;
 
 
 }
---
+++ Server/core/src/server/GameConstants.java
@@ -0,0 +1,7 @@
+package server;
+
+public abstract class GameConstants {
+    public static final int WIDTH     =   960;
+    public static final int HEIGHT    =   640;
+    public static final int BUFF_SIZE = 32768;
+}
